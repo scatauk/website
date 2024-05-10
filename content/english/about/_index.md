d097edfb2b6baba95fc4568871fc450f47c83249
JP Lomas <jp.lomas@gmail.com>
2024-05-10 15:04:47 +0100

    Hugo version

--- content/english/about/_index.md
+++ content/english/about/_index.md
@@ -1,9 +1,22 @@
 ---
-title: "Hey, I am John Doe!"
+title: "About the Society"
 meta_title: "About"
 description: "this is meta description"
-image: "/images/avatar.png"
+image: "/images/about-page.png"
 draft: false
 ---
 
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis illum nesciunt commodi vel nisi ut alias excepturi ipsum, totam, labore tempora, odit ex iste tempore sed. Fugit voluptatibus perspiciatis assumenda nulla ad nihil, omnis vel, doloremque sit quam autem optio maiores, illum eius facilis et quo consectetur provident dolor similique! Enim voluptatem dicta expedita veritatis repellat dolorum impedit, provident quasi at.
+Established in 1987, we are a specialist medical society and charity. Our aims are to:
+
+- Promote, fund and disseminate the results of research into the use of technology in anaesthesia and perioperative care
+- Promote and facilitate education and training in digital health, with a focus on perioperative care
+- Work in partnership with similar organisations such as ESCTAIC in Europe and the STA in the United States
+- Encourage engagement of clinicians in ISOs such as HL7-FHIR, OpenEHR and SNOMED-CT through membership and partnership
+
+#### Our membership
+
+Our members are a diverse group of current and former anaesthetists, intensivists, and perioperative physicians, as well as other healthcare professionals with an interest in digital health.
+
+#### Our membership network
+
+We use the Slack platform as a tool for communication and collaboration. All SCATA members are eligible to join the Slack workspace, where they can connect with other members, participate in discussions and working groups.
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,70 @@
+const fs = require("fs");
+const path = require("path");
+const themePath = path.join(__dirname, "data/theme.json");
+const themeRead = fs.readFileSync(themePath, "utf8");
+const theme = JSON.parse(themeRead);
+
+let font_base = Number(theme.fonts.font_size.base.replace("px", ""));
+let font_scale = Number(theme.fonts.font_size.scale);
+let h6 = font_scale;
+let h5 = h6 * font_scale;
+let h4 = h5 * font_scale;
+let h3 = h4 * font_scale;
+let h2 = h3 * font_scale;
+let h1 = h2 * font_scale;
+let fontPrimary, fontPrimaryType, fontSecondary, fontSecondaryType;
+if (theme.fonts.font_family.primary) {
+  fontPrimary = theme.fonts.font_family.primary
+    .replace(/\+/g, " ")
+    .replace(/:[ital,]*[ital@]*[wght@]*[0-9,;.]+/gi, "");
+  fontPrimaryType = theme.fonts.font_family.primary_type;
+}
+if (theme.fonts.font_family.secondary) {
+  fontSecondary = theme.fonts.font_family.secondary
+    .replace(/\+/g, " ")
+    .replace(/:[ital,]*[ital@]*[wght@]*[0-9,;.]+/gi, "");
+  fontSecondaryType = theme.fonts.font_family.secondary_type;
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./hugo_stats.json"],
+  safelist: [{ pattern: /^swiper-/ }],
   darkMode: "class",
   theme: {
+    screens: {
+      sm: "540px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     container: {
       center: true,
-      padding: "1rem",
-    },
-
-    screens: {
-      xs: "450px",
-      // => @media (min-width: 450px) { ... }
-
-      sm: "575px",
-      // => @media (min-width: 576px) { ... }
-
-      md: "768px",
-      // => @media (min-width: 768px) { ... }
-
-      lg: "992px",
-      // => @media (min-width: 992px) { ... }
-
-      xl: "1200px",
-      // => @media (min-width: 1200px) { ... }
-
-      "2xl": "1400px",
-      // => @media (min-width: 1400px) { ... }
+      padding: "2rem",
     },
     extend: {
       colors: {
-        current: "currentColor",
-        transparent: "transparent",
+        text: theme.colors.default.text_color.default,
+        light: theme.colors.default.text_color.light,
+        dark: theme.colors.default.text_color.dark,
+        primary: theme.colors.default.theme_color.primary,
+        secondary: theme.colors.default.theme_color.secondary,
+        body: theme.colors.default.theme_color.body,
+        border: theme.colors.default.theme_color.border,
+        "theme-light": theme.colors.default.theme_color.theme_light,
+        "theme-dark": theme.colors.default.theme_color.theme_dark,
+        darkmode: {
+          text: theme.colors.darkmode.text_color.default,
+          light: theme.colors.darkmode.text_color.light,
+          dark: theme.colors.darkmode.text_color.dark,
+          primary: theme.colors.darkmode.theme_color.primary,
+          secondary: theme.colors.darkmode.theme_color.secondary,
+          body: theme.colors.darkmode.theme_color.body,
+          border: theme.colors.darkmode.theme_color.border,
+          "theme-light": theme.colors.darkmode.theme_color.theme_light,
+          "theme-dark": theme.colors.darkmode.theme_color.theme_dark,
+        },
         white: "#FFFFFF",
         black: "#121723",
         primary: "#273D75",
@@ -48,25 +78,38 @@ module.exports = {
         "stroke-dark": "#E3E8EF",
         "bg-color-dark": "#171C28",
       },
-
-      boxShadow: {
-        signUp: "0px 5px 10px rgba(4, 10, 34, 0.2)",
-        one: "0px 2px 3px rgba(7, 7, 77, 0.05)",
-        two: "0px 5px 10px rgba(6, 8, 15, 0.1)",
-        three: "0px 5px 15px rgba(6, 8, 15, 0.05)",
-        sticky: "inset 0 -1px 0 0 rgba(0, 0, 0, 0.1)",
-        "sticky-dark": "inset 0 -1px 0 0 rgba(255, 255, 255, 0.1)",
-        "feature-2": "0px 10px 40px rgba(48, 86, 211, 0.12)",
-        submit: "0px 5px 20px rgba(4, 10, 34, 0.1)",
-        "submit-dark": "0px 5px 20px rgba(4, 10, 34, 0.1)",
-        btn: "0px 1px 2px rgba(4, 10, 34, 0.15)",
-        "btn-hover": "0px 1px 2px rgba(0, 0, 0, 0.15)",
-        "btn-light": "0px 1px 2px rgba(0, 0, 0, 0.1)",
+      fontSize: {
+        base: font_base + "px",
+        "base-sm": font_base * 0.8 + "px",
+        h1: h1 + "rem",
+        "h1-sm": h1 * 0.9 + "rem",
+        h2: h2 + "rem",
+        "h2-sm": h2 * 0.9 + "rem",
+        h3: h3 + "rem",
+        "h3-sm": h3 * 0.9 + "rem",
+        h4: h4 + "rem",
+        h5: h5 + "rem",
+        h6: h6 + "rem",
       },
-      dropShadow: {
-        three: "0px 5px 15px rgba(6, 8, 15, 0.05)",
+      fontFamily: {
+        primary: [fontPrimary, fontPrimaryType],
+        secondary: [fontSecondary, fontSecondaryType],
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("tailwind-bootstrap-grid")({
+      generateContainer: false,
+      gridGutterWidth: "2rem",
+      gridGutters: {
+        1: "0.25rem",
+        2: "0.5rem",
+        3: "1rem",
+        4: "1.5rem",
+        5: "3rem",
+      },
+    }),
+  ],
 };
