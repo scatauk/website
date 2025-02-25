9b9213b3c6c92a097e5523e85709e1db3fab0839
JP Lomas <jp.lomas@gmail.com>
2025-02-25 09:44:30 +0000

    Update vercel-build.sh

--- vercel-build.sh
+++ vercel-build.sh
@@ -22,7 +22,7 @@ export PATH=$PATH:/usr/local/go/bin
 rm -rf go${GO_VERSION}.linux-amd64.tar.gz
 go version
 
-# install Hugo
+# install Hugo 
 echo "Installing Hugo $HUGO_VERSION..."
 curl -sSOL https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
 tar -xzf hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
